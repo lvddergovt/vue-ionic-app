3270c7d00054d3c0c8076bafd605aaf608c58556
Guido Bakkes <gbakkes@gmail.com>
2022-03-02 21:18:37 +0100

    Add item and basic page

---
+++ vue-ionic/src/pages/MemoriesPage.vue
@@ -0,0 +1,35 @@
+<template>  
+	<ion-page>
+
+    <ion-header>
+      <ion-toolbar>
+        <ion-title>Test</ion-title>
+      </ion-toolbar>
+    </ion-header>
+
+    <ion-content>
+      <ion-list>
+        <ion-item>Item</ion-item>
+        <ion-item>Item</ion-item>
+        <ion-item>Item</ion-item>
+        <ion-item>Item</ion-item>
+      </ion-list>
+    </ion-content>
+
+  </ion-page>
+</template>
+
+<script>
+import { IonPage, IonHeader, IonTitle, IonContent, IonToolbar, IonList, IonItem } from "@ionic/vue";
+export default {
+  components: {
+    IonPage,
+    IonHeader,
+    IonTitle,
+    IonContent,
+    IonToolbar,
+    IonList,
+    IonItem
+  },
+};
+</script>
