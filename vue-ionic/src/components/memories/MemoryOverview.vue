f0b88f1a7b72cd4a342781cbd33551d9bbfd8051
Guido Bakkes <gbakkes@gmail.com>
2022-03-06 13:56:16 +0100

    Add detail page data

---
+++ vue-ionic/src/components/memories/MemoryOverview.vue
@@ -0,0 +1,18 @@
+<template>
+  <span>
+    <ion-img :src="image"></ion-img>
+    <h2 class="ion-text-center">{{ title }}</h2>
+    <p class="ion-text-center">{{ description }}</p>
+  </span>
+</template>
+
+<script>
+import { IonImg } from '@ionic/vue';
+
+export default {
+  props: ['title', 'image', 'description'],
+  components: {
+    IonImg 
+  }
+}
+</script>
